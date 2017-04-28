698faf23d0cc206b1be90d7bd077b69e42a0662a
gxx <562606139@qq.com>
2017-04-28 14:13:10 +0800

    4.28

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
 const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-	entry:{
-		a:'./src/script/main.js',
-		b:'./word.js'
+	entry: {
+		a: './src/script/main.js',
+		b: './word.js'
 	},
-	output:{
-		 path: path.resolve(__dirname, "dist/js"), // string
-		filename:'[name]-[chunkhash].js',
+	output: {
+		path: path.resolve(__dirname, "dist/js"), // string
+		filename: '[name]-[chunkhash].js',
 	},
+	plugins: [
+		new htmlWebpackPlugin(),
+	],
 }
