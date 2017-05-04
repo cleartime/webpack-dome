f1fe0f1f1d921a7bf2a0da81ada6815453dd1170
gxx <562606139@qq.com>
2017-05-04 23:28:51 +0800

    5.3

--- webpack.config.js
+++ webpack.config.js
@@ -10,9 +10,29 @@ module.exports = {
 	},
 	module: {
 		rules: [{
-			test: /\.js$/,
-			use: 'babel-loader'
-		}]
+        test: /\.js$/,
+        // these are matching conditions, each accepting a regular expression or string
+        // test and include have the same behavior, both must be matched
+        // exclude must not be matched (takes preferrence over test and include)
+        // Best practices:
+        // - Use RegExp only in test and for filename matching
+        // - Use arrays of absolute paths in include and exclude
+        // - Try to avoid exclude and prefer include
+
+        // conditions for the issuer (the origin of the import)
+
+        // flags to apply these rules, even if they are overridden (advanced option)
+
+        loader: "babel-loader",
+        // the loader which should be applied, it'll be resolved relative to the context
+        // -loader suffix is no longer optional in webpack2 for clarity reasons
+        // see webpack 1 upgrade guide
+
+        options: {
+          presets: ["es2015"]
+        },
+        // options for the loader
+      }]
 	},
 	plugins: [
 		new htmlWebpackPlugin({
